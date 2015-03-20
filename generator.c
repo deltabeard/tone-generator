758c0dd60edbafd398ccc6936047cfef18f4dfad
Stephan Ziep <stephan@ziep.com>
2015-03-20 17:02:10 +0100

    now possible to have tones generated shorter than a second

--- generator.c
+++ generator.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
 
 /* These are in charge of maintaining our sine function */
 float sinPos;
@@ -12,89 +13,109 @@ float sinStep;
 
 /* These are the audio card settings */
 #define FREQ 44100
-#define SAMPLES 8192 
+#define SAMPLES 8192
 
 /* This is basically an arbitrary number */
 #define VOLUME 127.0
+#define MILLI_TO_NANO 1000000.L
+#define MILLI 1000.L
 
 void populate(void* data, Uint8 *stream, int len) {
-	int i=0;
-	for (i=0; i<len; i++) {
-		/* Just fill the stream with sine! */
-		stream[i] = (Uint8) (VOLUME * sinf(sinPos))+127;
-		sinPos += sinStep;
-	}
+  int i=0;
+  for (i=0; i<len; i++) {
+    /* Just fill the stream with sine! */
+    stream[i] = (Uint8) (VOLUME * sinf(sinPos))+127;
+    sinPos += sinStep;
+  }
 }
 
 void usage() {
-	puts ("Usage: generator freq [duration]\nWhere:\n\tfreq is the frequency of the tone, given in Hz.\n\tduration is optional. It defines the number of seconds the tone should play for.\n\t\tIf not given, it plays for 1 second.");
+  puts ("Usage: generator freq [duration]\nWhere:\n\tfreq is the frequency of the tone, given in Hz.\n\tduration is optional. It defines the number of seconds the tone should play for.\n\t\tIf not given, it plays for 1 second.");
 }
 
 int main(int argc, char* argv[]) {
-	/* This will hold our data */
-	SDL_AudioSpec spec;
-	/* This will hold the requested frequency */
-	long reqFreq = 440;
-	/* This is the duration to hold the note for */
-	int duration = 1;
+  /* This will hold our data */
+  SDL_AudioSpec spec;
 
-	/* Process Command Line Arguments */
-	if (argc <= 1) {
-		/* Nothing Given, output usage */
-		usage();
-		exit(EXIT_FAILURE);
-	} else if (argc >= 2) {
-		/* Has frequency */
-		reqFreq = strtol(argv[1], NULL, 10);
-		if (errno == EINVAL) {
-			fprintf (stderr, "Frequency '%s' is invalid\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
+  struct timespec tim, tim2;
 
-		if (argc >= 3) {
-			/* Has duration */
-			duration = (int) strtol(argv[2], NULL, 10);
-			if (errno == EINVAL) {
-				fprintf (stderr,"Duration '%s' is invalid\n", argv[2]);
-				exit(EXIT_FAILURE);
-			}
-		}
+  /* This will hold the requested frequency */
+  long reqFreq = 440;
+  /* This is the duration to hold the note for */
+  long duration = 1000;
 
-		if (argc >=4) {
-			/* Who knows what's here */
-			puts ("Warning: Arguments found past frequency and duration, disregarding them\n");
-		}
-	} 
+  double sec = 0.0;
+  double nsec = 0;
 
-	/* Set up the requested settings */
-	spec.freq = FREQ;
-	spec.format = AUDIO_U8;
-	spec.channels = 1;
-	spec.samples = SAMPLES;
-	spec.callback = (*populate);
-	spec.userdata = NULL;
+  /* Process Command Line Arguments */
+  if (argc <= 1) {
+    /* Nothing Given, output usage */
+    usage();
+    exit(EXIT_FAILURE);
+  } else if (argc >= 2) {
+    /* Has frequency */
+    reqFreq = strtol(argv[1], NULL, 10);
+    if (errno == EINVAL) {
+      fprintf (stderr, "Frequency '%s' is invalid\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
 
-	/* Open the audio channel */
-	if (SDL_OpenAudio(&spec, NULL) < 0) { 
-		/* FAIL! */
-		fprintf(stderr, "Failed to open audio: %s \n", SDL_GetError());
-		exit(1); 
-	} 
+    if (argc >= 3) {
+      /* Has duration */
+      duration = strtol(argv[2], NULL, 10);
+      sec = (double) duration / MILLI;
+      nsec = ((int)duration % (int)MILLI) * MILLI_TO_NANO;
+      tim.tv_sec = (int)floor(sec);
+      tim.tv_nsec = (int) floor(nsec);
 
-	/* Initialize the position of our sine wave */
-	sinPos = 0;
-	/* Calculate the step of our sin wave */
-	sinStep = 2 * M_PI * reqFreq / FREQ;
+      // fprintf(stdout, "Time: %i sec, %i\n", (int)tim.tv_sec, (int)tim.tv_nsec);
 
-	/* Now, run this thing */
-	SDL_PauseAudio(0); 
-	/* Delay for the requested number of seconds */
-	sleep(duration);
-	/* Then turn it off again */
-	SDL_PauseAudio(1);
+      if (errno == EINVAL) {
+        fprintf (stderr,"Duration '%s' is invalid\n", argv[2]);
+        exit(EXIT_FAILURE);
+      }
+    }
 
-	/* Close audio channel */
-	SDL_CloseAudio();
+    if (argc >=4) {
+      /* Who knows what's here */
+      puts ("Warning: Arguments found past frequency and duration, disregarding them\n");
+    }
+  }
 
-	return 0;
+  /* Set up the requested settings */
+  spec.freq = FREQ;
+  spec.format = AUDIO_U8;
+  spec.channels = 1;
+  spec.samples = SAMPLES;
+  spec.callback = (*populate);
+  spec.userdata = NULL;
+
+  /* Open the audio channel */
+  if (SDL_OpenAudio(&spec, NULL) < 0) {
+    /* FAIL! */
+    fprintf(stderr, "Failed to open audio: %s \n", SDL_GetError());
+    exit(1);
+  }
+
+  /* Initialize the position of our sine wave */
+  sinPos = 0;
+  /* Calculate the step of our sin wave */
+  sinStep = 2 * M_PI * reqFreq / FREQ;
+
+  /* Now, run this thing */
+  SDL_PauseAudio(0);
+
+  if(nanosleep(&tim , &tim2) < 0 ) {
+    printf("Nano sleep system call failed \n");
+    return -1;
+  }
+
+  /* Then turn it off again */
+  SDL_PauseAudio(1);
+
+  /* Close audio channel */
+  SDL_CloseAudio();
+
+  return 0;
 }
+
