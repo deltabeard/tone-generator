5ee97edde7de82f8495e0fb681c10776316eff98
Mahyar Koshkouei <deltabeard@users.noreply.github.com>
2015-12-05 11:44:02 +0000

    Fix compile and code style changes
    Now compiles on Archlinux.
    Changed indents to use tabs.
    Max width of each line is now 80 characters.
    
    Signed-off-by: Mahyar Koshkouei <deltabeard@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: clean bundle
 
 generator: generator.c
-	$(CC) -o generator `sdl-config --cflags --libs` generator.c
+	$(CC) -o generator `sdl-config --cflags --libs` -lm generator.c
 
 bundle: generator
 	sh makeAppBundle
--- generator.c
+++ generator.c
@@ -21,101 +21,102 @@ float sinStep;
 #define MILLI 1000.L
 
 void populate(void* data, Uint8 *stream, int len) {
-  int i=0;
-  for (i=0; i<len; i++) {
-    /* Just fill the stream with sine! */
-    stream[i] = (Uint8) (VOLUME * sinf(sinPos))+127;
-    sinPos += sinStep;
-  }
+	int i=0;
+	for (i=0; i<len; i++) {
+		/* Just fill the stream with sine! */
+		stream[i] = (Uint8) (VOLUME * sinf(sinPos))+127;
+		sinPos += sinStep;
+	}
 }
 
 void usage() {
-  puts ("Usage: generator freq [duration]\nWhere:\n\tfreq is the frequency of the tone, given in Hz.\n\tduration is optional. It defines the number of seconds the tone should play for.\n\t\tIf not given, it plays for 1 second.");
+	puts ("Usage: generator freq [duration]\nWhere:\n\tfreq is the"
+		"frequency of the tone, given in Hz.\n\tduration is optional."
+		"It defines the number of seconds the tone should play for.\n"
+		"\t\tIf not given, it plays for 1 second.");
 }
 
 int main(int argc, char* argv[]) {
-  /* This will hold our data */
-  SDL_AudioSpec spec;
+	/* This will hold our data */
+	SDL_AudioSpec spec;
 
-  struct timespec tim, tim2;
+	struct timespec tim, tim2;
 
-  /* This will hold the requested frequency */
-  long reqFreq = 440;
-  /* This is the duration to hold the note for */
-  long duration = 1000;
+	/* This will hold the requested frequency */
+	long reqFreq = 440;
+	/* This is the duration to hold the note for */
+	long duration = 1000;
 
-  double sec = 0.0;
-  double nsec = 0;
+	double sec = 0.0;
+	double nsec = 0;
 
-  /* Process Command Line Arguments */
-  if (argc <= 1) {
-    /* Nothing Given, output usage */
-    usage();
-    exit(EXIT_FAILURE);
-  } else if (argc >= 2) {
-    /* Has frequency */
-    reqFreq = strtol(argv[1], NULL, 10);
-    if (errno == EINVAL) {
-      fprintf (stderr, "Frequency '%s' is invalid\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
+	/* Process Command Line Arguments */
+	if (argc <= 1) {
+		/* Nothing Given, output usage */
+		usage();
+		exit(EXIT_FAILURE);
+	} else if (argc >= 2) {
+		/* Has frequency */
+		reqFreq = strtol(argv[1], NULL, 10);
+		if (errno == EINVAL) {
+			fprintf (stderr, "Frequency '%s' is invalid\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
 
-    if (argc >= 3) {
-      /* Has duration */
-      duration = strtol(argv[2], NULL, 10);
-      sec = (double) duration / MILLI;
-      nsec = ((int)duration % (int)MILLI) * MILLI_TO_NANO;
-      tim.tv_sec = (int)floor(sec);
-      tim.tv_nsec = (int) floor(nsec);
+		if (argc >= 3) {
+			/* Has duration */
+			duration = strtol(argv[2], NULL, 10);
+			sec = (double) duration / MILLI;
+			nsec = ((int)duration % (int)MILLI) * MILLI_TO_NANO;
+			tim.tv_sec = (int)floor(sec);
+			tim.tv_nsec = (int) floor(nsec);
 
-      // fprintf(stdout, "Time: %i sec, %i\n", (int)tim.tv_sec, (int)tim.tv_nsec);
+			if (errno == EINVAL) {
+				fprintf (stderr,"Duration '%s' is invalid\n", argv[2]);
+				exit(EXIT_FAILURE);
+			}
+		}
 
-      if (errno == EINVAL) {
-        fprintf (stderr,"Duration '%s' is invalid\n", argv[2]);
-        exit(EXIT_FAILURE);
-      }
-    }
+		if (argc >=4) {
+			/* Who knows what's here */
+			puts ("Warning: Arguments found past frequency and"
+				"duration, disregarding them\n");
+		}
+	}
 
-    if (argc >=4) {
-      /* Who knows what's here */
-      puts ("Warning: Arguments found past frequency and duration, disregarding them\n");
-    }
-  }
+	/* Set up the requested settings */
+	spec.freq = FREQ;
+	spec.format = AUDIO_U8;
+	spec.channels = 1;
+	spec.samples = SAMPLES;
+	spec.callback = (*populate);
+	spec.userdata = NULL;
 
-  /* Set up the requested settings */
-  spec.freq = FREQ;
-  spec.format = AUDIO_U8;
-  spec.channels = 1;
-  spec.samples = SAMPLES;
-  spec.callback = (*populate);
-  spec.userdata = NULL;
+	/* Open the audio channel */
+	if (SDL_OpenAudio(&spec, NULL) < 0) {
+		/* FAIL! */
+		fprintf(stderr, "Failed to open audio: %s \n", SDL_GetError());
+		exit(1);
+	}
 
-  /* Open the audio channel */
-  if (SDL_OpenAudio(&spec, NULL) < 0) {
-    /* FAIL! */
-    fprintf(stderr, "Failed to open audio: %s \n", SDL_GetError());
-    exit(1);
-  }
+	/* Initialize the position of our sine wave */
+	sinPos = 0;
+	/* Calculate the step of our sin wave */
+	sinStep = 2 * M_PI * reqFreq / FREQ;
 
-  /* Initialize the position of our sine wave */
-  sinPos = 0;
-  /* Calculate the step of our sin wave */
-  sinStep = 2 * M_PI * reqFreq / FREQ;
+	/* Now, run this thing */
+	SDL_PauseAudio(0);
 
-  /* Now, run this thing */
-  SDL_PauseAudio(0);
+	if(nanosleep(&tim , &tim2) < 0 ) {
+		printf("Nano sleep system call failed \n");
+		return -1;
+	}
 
-  if(nanosleep(&tim , &tim2) < 0 ) {
-    printf("Nano sleep system call failed \n");
-    return -1;
-  }
+	/* Then turn it off again */
+	SDL_PauseAudio(1);
 
-  /* Then turn it off again */
-  SDL_PauseAudio(1);
+	/* Close audio channel */
+	SDL_CloseAudio();
 
-  /* Close audio channel */
-  SDL_CloseAudio();
-
-  return 0;
+	return 0;
 }
-
