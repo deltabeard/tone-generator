53d2dae15e3f14b8c6a5590b39558f9493ed1e8e
Mahyar Koshkouei <deltabeard@users.noreply.github.com>
2015-12-05 12:09:48 +0000

    Add super_mario.sh example
    Converted already available super mario example for use with bash.
    
    Signed-off-by: Mahyar Koshkouei <deltabeard@users.noreply.github.com>

---
+++ super_mario.sh
@@ -0,0 +1,350 @@
+./generator 660 100
+sleep 0.150
+./generator 660 100
+sleep 0.300
+./generator 660 100
+sleep 0.300
+./generator 510 100
+sleep 0.100
+./generator 660 100
+sleep 0.300
+./generator 770 100
+sleep 0.550
+./generator 380 100
+sleep 0.575
+./generator 510 100
+sleep 0.450
+./generator 380 100
+sleep 0.400
+./generator 320 100
+sleep 0.500
+./generator 440 100
+sleep 0.300
+./generator 480 80
+sleep 0.330
+./generator 450 100
+sleep 0.150
+./generator 430 100
+sleep 0.300
+./generator 380 100
+sleep 0.200
+./generator 660 80
+sleep 0.200
+./generator 760 50
+sleep 0.150
+./generator 860 100
+sleep 0.300
+./generator 700 80
+sleep 0.150
+./generator 760 50
+sleep 0.350
+./generator 660 80
+sleep 0.300
+./generator 520 80
+sleep 0.150
+./generator 580 80
+sleep 0.150
+./generator 480 80
+sleep 0.500
+
+./generator 510 100
+sleep 0.450
+./generator 380 100
+sleep 0.400
+./generator 320 100
+sleep 0.500
+./generator 440 100
+sleep 0.300
+./generator 480 80
+sleep 0.330
+./generator 450 100
+sleep 0.150
+./generator 430 100
+sleep 0.300
+./generator 380 100
+sleep 0.200
+./generator 660 80
+sleep 0.200
+./generator 760 50
+sleep 0.150
+./generator 860 100
+sleep 0.300
+./generator 700 80
+sleep 0.150
+./generator 760 50
+sleep 0.350
+./generator 660 80
+sleep 0.300
+./generator 520 80
+sleep 0.150
+./generator 580 80
+sleep 0.150
+./generator 480 80
+sleep 0.500
+
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 150
+sleep 0.300
+./generator 380 100
+sleep 0.150
+./generator 430 100
+sleep 0.150
+
+./generator 500 100
+sleep 0.300
+./generator 430 100
+sleep 0.150
+./generator 500 100
+sleep 0.100
+./generator 570 100
+sleep 0.220
+
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 200
+sleep 0.300
+
+./generator 1020 80
+sleep 0.300
+./generator 1020 80
+sleep 0.150
+./generator 1020 80
+sleep 0.300
+
+./generator 380 100
+sleep 0.300
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 150
+sleep 0.300
+./generator 380 100
+sleep 0.150
+./generator 430 100
+sleep 0.150
+
+./generator 500 100
+sleep 0.300
+./generator 430 100
+sleep 0.150
+./generator 500 100
+sleep 0.100
+./generator 570 100
+sleep 0.420
+
+./generator 585 100
+sleep 0.450
+
+./generator 550 100
+sleep 0.420
+
+./generator 500 100
+sleep 0.360
+
+./generator 380 100
+sleep 0.300
+./generator 500 100
+sleep 0.300
+./generator 500 100
+sleep 0.150
+./generator 500 100
+sleep 0.300
+
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 150
+sleep 0.300
+./generator 380 100
+sleep 0.150
+./generator 430 100
+sleep 0.150
+
+./generator 500 100
+sleep 0.300
+./generator 430 100
+sleep 0.150
+./generator 500 100
+sleep 0.100
+./generator 570 100
+sleep 0.220
+
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 200
+sleep 0.300
+
+./generator 1020 80
+sleep 0.300
+./generator 1020 80
+sleep 0.150
+./generator 1020 80
+sleep 0.300
+
+./generator 380 100
+sleep 0.300
+./generator 500 100
+sleep 0.300
+
+./generator 760 100
+sleep 0.100
+./generator 720 100
+sleep 0.150
+./generator 680 100
+sleep 0.150
+./generator 620 150
+sleep 0.300
+
+./generator 650 150
+sleep 0.300
+./generator 380 100
+sleep 0.150
+./generator 430 100
+sleep 0.150
+
+./generator 500 100
+sleep 0.300
+./generator 430 100
+sleep 0.150
+./generator 500 100
+sleep 0.100
+./generator 570 100
+sleep 0.420
+
+./generator 585 100
+sleep 0.450
+
+./generator 550 100
+sleep 0.420
+
+./generator 500 100
+sleep 0.360
+
+./generator 380 100
+sleep 0.300
+./generator 500 100
+sleep 0.300
+./generator 500 100
+sleep 0.150
+./generator 500 100
+sleep 0.300
+
+./generator 500 60
+sleep 0.150
+./generator 500 80
+sleep 0.300
+./generator 500 60
+sleep 0.350
+./generator 500 80
+sleep 0.150
+./generator 580 80
+sleep 0.350
+./generator 660 80
+sleep 0.150
+./generator 500 80
+sleep 0.300
+./generator 430 80
+sleep 0.150
+./generator 380 80
+sleep 0.600
+
+./generator 500 60
+sleep 0.150
+./generator 500 80
+sleep 0.300
+./generator 500 60
+sleep 0.350
+./generator 500 80
+sleep 0.150
+./generator 580 80
+sleep 0.150
+./generator 660 80
+sleep 0.550
+
+./generator 870 80
+sleep 0.325
+./generator 760 80
+sleep 0.600
+
+./generator 500 60
+sleep 0.150
+./generator 500 80
+sleep 0.300
+./generator 500 60
+sleep 0.350
+./generator 500 80
+sleep 0.150
+./generator 580 80
+sleep 0.350
+./generator 660 80
+sleep 0.150
+./generator 500 80
+sleep 0.300
+./generator 430 80
+sleep 0.150
+./generator 380 80
+sleep 0.600
+
+./generator 660 100
+sleep 0.150
+./generator 660 100
+sleep 0.300
+./generator 660 100
+sleep 0.300
+./generator 510 100
+sleep 0.100
+./generator 660 100
+sleep 0.300
+./generator 770 100
+sleep 0.550
+./generator 380 100
+sleep 0.575
